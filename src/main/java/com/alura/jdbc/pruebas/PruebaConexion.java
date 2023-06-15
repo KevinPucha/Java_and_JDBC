c2ca135db2c89a3fa22667c2a6d0407f474a3b0c
Kevin Pucha <kevinpucha197a@gmail.com>
2023-06-15 10:52:59 -0500

    Deleting comments and non used libraries

--- src/main/java/com/alura/jdbc/pruebas/PruebaConexion.java
+++ src/main/java/com/alura/jdbc/pruebas/PruebaConexion.java
@@ -1,7 +1,6 @@
 package com.alura.jdbc.pruebas;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.alura.jdbc.factory.ConnectionFactory;
@@ -11,13 +10,6 @@ public class PruebaConexion {
     public static void main(String[] args) throws SQLException {
     	
     	Connection con = new ConnectionFactory().recuperaConexion();
-    	
-//	Here we replace the way that we stablish a conexion, insted we have created one class for that    	
-//    	String url = "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC"; 
-//      String userName = "root";
-//      String passWord = "KyD031014";
-//    	
-//    	Connection con = DriverManager.getConnection(url,userName,passWord);
                 
         System.out.println("Cerrando la conexión");
 
